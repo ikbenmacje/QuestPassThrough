92a34591a7402b7a2626679f38d46652b1cdbef7
ikbenmacje <machiel@z25.org>
2023-10-19 12:30:03 +0200

    Initial Commit

---
+++ Assets/WebcamFeed.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamDualEyed : MonoBehaviour
+{
+
+    public GameObject plane;
+    //Second Camera Added by Meg
+  
+    public int cameraNumber = 3;
+
+    private WebCamTexture webCamTexture;
+    private WebCamDevice webCamDevice;
+
+    void Start()
+    {
+        // Checks how many and which cameras are available on the device
+        for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++)
+        {
+            Debug.Log("WebCamDevice " + cameraIndex + " name " + WebCamTexture.devices[cameraIndex].name);
+        }
+
+        if (WebCamTexture.devices.Length > cameraNumber)
+        {
+            webCamDevice = WebCamTexture.devices[cameraNumber];
+            webCamTexture = new WebCamTexture(webCamDevice.name, 1280, 720);
+            plane.GetComponent<Renderer>().material.mainTexture = webCamTexture;
+           
+            webCamTexture.Play();
+        }
+        else
+        {
+            Debug.Log("no camera");
+        }
+    }
+}
